381ec422e8606f61c9566a3baec93d9fece8de94
thekingking <1677273255@qq.com>
2024-05-18 15:15:16 +0800

    前缀和

--- 前缀和/基础/2024-05-18-m_2438.rs
+++ 前缀和/基础/2024-05-18-m_2438.rs
@@ -1,20 +1,12 @@
-
-
-fn main() {
-    let mut powers = vec![];
-    let mut n = 323243;
-    while n != 0 {
-        let lowbit = n & (-n);
-        powers.push(lowbit);
-        n ^= lowbit;
-    }
-    println!("{:?}", powers);
-}
-
 struct Solution;
 
 impl Solution {
     
+    // 由于Rust数值类型长度限制，所以分段求余数然后再相乘
+    // 本题主要是考察二进制，powers中为n中二进制每一位，将每一位取出即可计算，
+    // 可使用前缀和进行优化，考虑到需要进行类型转换太麻烦了就没做了
+    // 个人认为本题难点在数值计算中的取余操作，由于有长度限制，不方便通过二进制位数一次操作完成
+
     pub fn product_queries(n: i32, queries: Vec<Vec<i32>>) -> Vec<i32> {
         const MOD:i64 = 1_000_000_007;
         let mut powers = vec![];
