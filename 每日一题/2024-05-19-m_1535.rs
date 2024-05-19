d4f7e8b7f5b9443b2680aeb5d2fd43ae2024243d
thekingking <1677273255@qq.com>
2024-05-19 09:59:00 +0800

    每日一题

--- 每日一题/2024-05-19-m_1535.rs
+++ 每日一题/2024-05-19-m_1535.rs
@@ -1,14 +1,10 @@
-
-
-fn main() {
-    println!("hello, world");
-}
-
-#[warn(dead_code)]
 struct Solution;
 
 impl Solution {
     
+    // 又一个双百
+    // 迭代器访问数据比通过下标访问更快
+    // 这个题应该是简单题才对
     pub fn get_winner(arr: Vec<i32>, k: i32) -> i32 {
         let mut max = arr[0];
         let mut num = 0;
