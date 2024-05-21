219505e52c4ea9c4fd875ec35a73990374f9ed03
thekingking <1677273255@qq.com>
2024-05-21 10:46:55 +0800

    每日一题，今天521，简单题也太简单了吧

--- 每日一题/2024-05-21-e_2769.rs
+++ 每日一题/2024-05-21-e_2769.rs
@@ -1,12 +1,7 @@
-use std::collections::HashMap;
-fn main() {
-    println!("hello, world");
-}
-
-#[warn(dead_code)]
 struct Solution;
 
 impl Solution {
+    /// 简单题，太简单了
     pub fn the_maximum_achievable_x(num: i32, t: i32) -> i32 {
         num + t * 2
     }
