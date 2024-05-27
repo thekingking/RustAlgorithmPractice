531842f7d79779d20bf8e40f4c12d829baad22d8
thekingking <1677273255@qq.com>
2024-05-27 11:50:56 +0800

    前缀和

--- 前缀和/基础/2024-05-27-m_53.rs
+++ 前缀和/基础/2024-05-27-m_53.rs
@@ -1,11 +1,7 @@
-fn main() {
-    println!("hello, world");
-}
-
-#[warn(dead_code)]
 struct Solution;
 
 impl Solution {
+    // 前缀和，也是妙到家了
     pub fn max_sub_array(nums: Vec<i32>) -> i32 {
         let mut ans = nums[0];
         let mut pre = 0;
@@ -16,5 +12,16 @@ impl Solution {
             min_pre = pre.min(min_pre);
         }
         ans
+    }
+
+    // 动态规划，妙到家了
+    pub fn max_sub_array(nums: Vec<i32>) -> i32 {
+        let mut pre = 0;
+        let mut max = nums[0];
+        for n in nums {
+            pre = n.max(pre + n);
+            max = max.max(pre);
+        }
+        max
     }
 }
