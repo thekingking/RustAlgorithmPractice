0b5ed4809e01bb8ca428384239a0cce2fa13edfa
thekingking <1677273255@qq.com>
2024-05-28 13:31:12 +0800

    前缀和与哈希表

--- 前缀和/前缀和与哈希表/2024-05-28-m_930.rs
+++ 前缀和/前缀和与哈希表/2024-05-28-m_930.rs
@@ -1,13 +1,22 @@
 use std::collections::HashMap;
-
-fn main() {
-    println!("hello, world");
-}
-
-#[warn(dead_code)]
 struct Solution;
 
 impl Solution {
+    // 哈希表
+    pub fn num_subarrays_with_sum(nums: Vec<i32>, goal: i32) -> i32 {
+        let mut cnt = HashMap::new();
+        let mut ans = 0;
+        let mut sum = 0;
+        for i in 0..nums.len() {
+            cnt.entry(sum).and_modify(|x| *x += 1).or_insert(1);
+            sum += nums[i];
+            ans += cnt.get(&(sum - goal)).unwrap_or(&0);
+        }
+        ans
+    }
+
+    // 滑动窗口
+    // 滑动窗口右边一步一步移动，左边按范围条件移动，由后向前寻找
     pub fn num_subarrays_with_sum(nums: Vec<i32>, goal: i32) -> i32 {
         let (mut left1, mut left2, mut right) = (0, 0, 0);
         let (mut sum1, mut sum2) = (0, 0);
--- 每日一题/2024-05-28-e_2951.rs
+++ 每日一题/2024-05-28-e_2951.rs
@@ -1,6 +1,7 @@
 struct Solution;
 
 impl Solution {
+    // 简单题
     pub fn find_peaks(mountain: Vec<i32>) -> Vec<i32> {
         let mut ans = Vec::new();
         for i in 1..(mountain.len() - 1) {
