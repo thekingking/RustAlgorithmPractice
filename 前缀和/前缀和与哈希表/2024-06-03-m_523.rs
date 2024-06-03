507551848b0a066a4a3aabd8e4b2bb07492f9814
thekingking <1677273255@qq.com>
2024-06-03 13:29:23 +0800

    距离和

--- 前缀和/前缀和与哈希表/2024-06-03-m_523.rs
+++ 前缀和/前缀和与哈希表/2024-06-03-m_523.rs
@@ -2,11 +2,13 @@ struct Solution;
 
 impl Solution {
     /// 跟974题差不多，同样前缀和加哈希表，哈希表存储余数最小坐标，利用同余定理进行判断
+    /// 官解有问题，哈希表中存储最小下标即可
     pub fn check_subarray_sum(nums: Vec<i32>, k: i32) -> bool {
         let mut cnt = HashMap::new();
         let mut pre_sum = 0;
         let mut pre_index = -1;
         for x in nums {
+            // 存储最小下标
             cnt.entry(pre_sum).or_insert(pre_index);
             pre_sum = (pre_sum + x) % k;
             pre_index += 1;
--- 前缀和/距离和/2024-06-03-m_1685.rs
+++ 前缀和/距离和/2024-06-03-m_1685.rs
@@ -1,20 +1,15 @@
-use std::collections::HashMap;
-
-fn main() {
-    println!("hello, world");
-}
-
-#[warn(dead_code)]
 struct Solution;
 
 impl Solution {
+    /// 前缀和与总和，计算两侧总和，左侧都小于nums[i]，右侧都大于nums[i]
+    /// 双百
     pub fn get_sum_absolute_differences(nums: Vec<i32>) -> Vec<i32> {
         let sum = nums.iter().sum::<i32>();
         let n = nums.len() as i32;
         let mut pre_sum = 0;
         let mut ans = vec![0; nums.len()];
         for (i, x) in nums.iter().enumerate() {
-            ans[i] = x * i as i32 - pre_sum + (sum - pre_sum - x) - x * (n - i as i32 - 1);
+            ans[i] = sum - pre_sum * 2 + x * (2 * i as i32 - n);
             pre_sum += x;
         }
         ans
