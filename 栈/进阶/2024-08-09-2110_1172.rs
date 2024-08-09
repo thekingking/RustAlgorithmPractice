5c655523692aa0f1e70167378ce7473048ab294c
thekingking <yinjinsong365@gmail.com>
2024-08-09 12:35:40 +0800

    栈

--- 栈/进阶/2024-08-09-2110_1172.rs
+++ 栈/进阶/2024-08-09-2110_1172.rs
@@ -1,29 +1,3 @@
-use std::collections::HashMap;
-
-
-fn main() {
-    println!("hello, world");
-}
-
-struct Solution;
-
-impl Solution {
-    pub fn minimum_added_integer(mut nums1: Vec<i32>, mut nums2: Vec<i32>) -> i32 {
-        nums1.sort_unstable();
-        nums2.sort_unstable();
-        for i in (1..3).rev() {
-            let x = nums2[0] - nums1[i];
-            let mut j = 0;
-            for &v in &nums1[i..] {
-                if nums2[j] == v + x && { j += 1; j == nums2.len() } {
-                    return x;
-                }
-            }
-        }
-        nums2[0] - nums1[0]
-    }
-}
-
 struct DinnerPlates {
     capacity: usize,
     stacks: Vec<Vec<i32>>,
@@ -87,3 +61,12 @@ impl DinnerPlates {
         x
     }
 }
+
+
+/**
+ * Your DinnerPlates object will be instantiated and called as such:
+ * let obj = DinnerPlates::new(capacity);
+ * obj.push(val);
+ * let ret_2: i32 = obj.pop();
+ * let ret_3: i32 = obj.pop_at_stack(index);
+ */
