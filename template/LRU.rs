e7d89d165396b27cc7674d02be4170f7f8f779ed
thekingking <yinjinsong365@gmail.com>
2024-10-06 10:28:09 +0800

    LRU policy

--- template/LRU.rs
+++ template/LRU.rs
@@ -1,37 +1,3 @@
-use std::borrow::BorrowMut;
-use std::i32;
-
-
-
-fn main() {
-    println!("hello, world");
-}
-
-struct Solution;
-
-impl Solution {
-    pub fn minimum_time(time: Vec<i32>, total_trips: i32) -> i64 {
-        let mut left = 0;
-        let mut right = 10_i64.pow(14);
-        while left < right {
-            let mid = (left + right) / 2;
-            let mut sum = 0;
-            for &t in &time {
-                sum += mid / t as i64;
-                if sum > total_trips as i64{
-                    break;
-                }
-            }
-            if sum >= total_trips as i64 {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
-        }
-        left
-    }
-}
-
 use std::collections::HashMap;
 use std::cell::RefCell;
 use std::rc::Rc;
